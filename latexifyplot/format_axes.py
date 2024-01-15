f30522e01b80bcc339b09ac4fac827f25b7c98d2
prince7003 <princekumartmsl@gmail.com>
2024-01-15 20:16:38 +0530

    Initial commit

---
+++ latexifyplot/format_axes.py
@@ -0,0 +1,21 @@
+import math
+import matplotlib
+
+
+
+def format_axes(ax):
+  if args.latex:
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_color(SPINE_COLOR)
+        ax.spines[spine].set_linewidth(0.5)
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_tick_params(direction='out', color=SPINE_COLOR)
+
+  return ax
---
+++ latexifyplot/latexify.py
@@ -0,0 +1,50 @@
+import math
+import matplotlib
+
+def latexify(fig_width=None, fig_height=None):
+  if args.latex:
+    """Set up matplotlib's RC params for LaTeX plotting.
+    Call this before plotting a figure.
+
+    Parameters
+    ----------
+    fig_width : float, optional, inches
+    fig_height : float,  optional, inches
+    """
+
+    # code adapted from http://www.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
+
+    # Width and max height in inches for IEEE journals taken from
+    # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
+
+    fig_width_pt = 397.48499   # Get this from LaTeX using \the\textwidth
+    inches_per_pt = 1.0/72.27  # Convert pt to inch
+
+    if fig_width is None:
+        fig_width = fig_width_pt*inches_per_pt
+
+    if fig_height is None:
+        golden_mean = (math.sqrt(5)-1.0)/2.0    # Aesthetic ratio
+        fig_height = fig_width*golden_mean # height in inches
+
+    MAX_HEIGHT_INCHES = 8.0
+    if fig_height > MAX_HEIGHT_INCHES:
+        print("WARNING: fig_height too large:" + fig_height +
+              "so will reduce to" + MAX_HEIGHT_INCHES + "inches.")
+        fig_height = MAX_HEIGHT_INCHES
+
+    params = {
+      'backend': 'ps',
+      #'text.latex.preamble': ['\usepackage{gensymb}'],
+      'axes.labelsize': 8, # fontsize for x and y labels (was 10)
+      'axes.titlesize': 8,
+      'font.size':       8, # was 10
+      'legend.fontsize': 8, # was 10
+      'xtick.labelsize': 8,
+      'ytick.labelsize': 8,
+      'text.usetex': True,
+      'figure.figsize': [fig_width,fig_height],
+      'font.family': 'serif'
+    }
+
+    matplotlib.rcParams.update(params)
